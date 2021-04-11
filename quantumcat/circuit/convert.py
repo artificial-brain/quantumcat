b7c1b593aa71d65c236358c7bd53751f39f99b33
ab-jiteshlalwani <jitesh.lalwani@artificialbrain.in>
2021-04-12 00:05:10 +0530

    Added draw and execute functionality for google cirq

--- quantumcat/circuit/convert.py
+++ quantumcat/circuit/convert.py
@@ -1,24 +1,36 @@
 from qiskit import QuantumCircuit
 from quantumcat.utils import gates_map
 from quantumcat.circuit.op_type import OpType
+import cirq
 
 
 def to_qiskit(q_circuit, qubits, cbits):
     operations = q_circuit.operations
     qiskit_qc = QuantumCircuit(qubits, cbits)
-    for index in operations:
-        operation = next(iter(index.items()))
+    for op in operations:
+        operation = next(iter(op.items()))
         qiskit_op = gates_map.quantumcat_to_qiskit[operation[1]]
         qargs = operation[0]
         if qiskit_op == OpType.measure:
-            qiskit_qc.measure([qargs], [index['cbit']])
+            qiskit_qc.measure([qargs], [op['cbit']])
         else:
             qiskit_qc.append(qiskit_op(), [qargs])
     return qiskit_qc
 
 
 def to_cirq(q_circuit, qubits, cbits):
-    pass
+    operations = q_circuit.operations
+    cirq_qc = cirq.Circuit()
+    lined_qubits = cirq.LineQubit.range(qubits)
+    for op in operations:
+        operation = next(iter(op.items()))
+        qiskit_op = gates_map.quantumcat_to_cirq[operation[1]]
+        qargs = operation[0]
+        if qiskit_op == OpType.measure:
+            cirq_qc.append(cirq.ops.measure(lined_qubits[qargs]))
+        else:
+            cirq_qc.append([qiskit_op(lined_qubits[qargs])])
+    return cirq_qc
 
 
 def to_q_sharp(q_circuit, qubits, cbits):
--- quantumcat/main.py
+++ quantumcat/main.py
@@ -1,4 +1,5 @@
 from quantumcat.circuit import QCircuit
+from quantumcat.utils import providers
 
 if __name__ == '__main__':
     circuit = QCircuit(2, 2)
@@ -8,5 +9,5 @@ if __name__ == '__main__':
     circuit.x_gate(1)
     circuit.measure(0, 0)
     circuit.measure(1, 1)
-    circuit.draw_circuit()
-    print(circuit.execute())
+    circuit.draw_circuit(provider=providers.GOOGLE_PROVIDER)
+    print(circuit.execute(provider=providers.GOOGLE_PROVIDER, repetitions=10))
