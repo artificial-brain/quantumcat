3a64c8188943ca3a552a361807ef5d7577314d7e
ab-jiteshlalwani <jitesh.lalwani@artificialbrain.in>
2021-04-13 16:55:32 +0530

    Implemented multi-gate support for cirq

--- quantumcat/main.py
+++ quantumcat/main.py
@@ -1,14 +1,14 @@
 from quantumcat.circuit import QCircuit
 from quantumcat.utils import providers
 
+
 if __name__ == '__main__':
-    circuit = QCircuit(2, 2)
+    circuit = QCircuit(3, 2)
     circuit.x_gate(0)
-    # circuit.y_gate(0)
-    # circuit.z_gate(1)
-    circuit.x_gate(1)
+    # circuit.x_gate(1)
     circuit.cx_gate(0, 1)
+    circuit.x_gate(2)
     circuit.measure(0, 0)
     circuit.measure(1, 1)
     circuit.draw_circuit(provider=providers.GOOGLE_PROVIDER)
-    # print(circuit.execute(provider=providers.GOOGLE_PROVIDER, repetitions=10))
+    print(circuit.execute(provider=providers.GOOGLE_PROVIDER, repetitions=10))
