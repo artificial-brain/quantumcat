d1ff57a6fc9c97f3e837e017ae55a9d6843a6a39
instructorbj <>
2021-04-13 23:44:44 +0530

    Added I Gate

--- quantumcat/main.py
+++ quantumcat/main.py
@@ -3,14 +3,17 @@ from quantumcat.utils import providers
 
 
 if __name__ == '__main__':
-    circuit = QCircuit(3, 2)
+    circuit = QCircuit(3, 3)
     circuit.x_gate(0)
     circuit.h_gate(1)
+    circuit.y_gate(1)
     circuit.i_gate(1)
     #circuit.cx_gate(0, 1)
-    #circuit.x_gate(2)
+    circuit.x_gate(2)
+    circuit.i_gate(2)
     circuit.measure(0, 0)
     circuit.measure(1, 1)
+    circuit.measure(2, 2)
     circuit.draw_circuit(provider=providers.GOOGLE_PROVIDER)
     circuit.draw_circuit(provider=providers.IBM_PROVIDER)
     print(circuit.execute(provider=providers.GOOGLE_PROVIDER, repetitions=10))
