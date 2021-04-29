d908818480da746a07c0e885a58d0b72116265a8
Melvin Varghese <melvin.thomas@artificialbrain.in>
2021-04-29 17:27:55 +0530

    Added custom gates to cirq

---
+++ quantumcat/gates/custom_gates/cirq/csx_gate.py
@@ -0,0 +1,20 @@
+import cirq
+import numpy
+
+
+class CSXGate(cirq.Gate):
+
+    def __init__(self):
+        super(CSXGate, self).__init__()
+
+    def _num_qubits_(self):
+        return 2
+
+    def _unitary_(self, dtype=None):
+        par1 = (1+1j)/2
+        par2 = (1-1j)/2
+        return numpy.array([[1, 0, 0, 0], [0, par1, 0, par2], [0, 0, 1, 0], [0, par2, 0, par1]],
+                           dtype=dtype)
+    
+    def _circuit_diagram_info_(self, args):
+        return ["CSXGate"] * self.num_qubits()
